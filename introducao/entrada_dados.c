a808c5395d612428cc572b69571b3b07eb9aa9df
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-02-07 09:30:30 -0300

    Entrada de Dados

---
+++ introducao/entrada_dados.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("\nDigite um número: ");
+    scanf("%d", &num);
+    printf("O valor da variável 'num' é: %d\n", num);
+
+    return 0;
+}
